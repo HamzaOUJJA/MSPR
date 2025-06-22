9acc8b32504097c71bfd28b1ab9ca6c18941af16
Hamza OUJJA <hamzaoujja08@gmail.com>
2025-06-22 16:54:58 +0200

    Added Dockerfile

--- src/main.py
+++ src/main.py
@@ -10,18 +10,18 @@ if __name__ == "__main__":
     os.environ['SPARK_LOCAL_IP'] = '127.0.0.1'
     
     spark = SparkSession.builder \
-        .appName("GrabAndDumpJob") \
-        .master("spark://localhost:7077") \
-        .config("spark.submit.deployMode", "client") \
-        .config("spark.driver.host", "host.docker.internal") \
-        .config("spark.driver.bindAddress", "0.0.0.0") \
-        .config("spark.executor.memory", "2g") \
-        .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
-        .config("spark.hadoop.fs.s3a.access.key", "minio") \
-        .config("spark.hadoop.fs.s3a.secret.key", "minio123") \
-        .config("spark.hadoop.fs.s3a.path.style.access", "true") \
-        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.4") \
-        .getOrCreate()
+    .appName("GrabAndDumpJob") \
+    .master("spark://spark-master:7077") \
+    .config("spark.submit.deployMode", "client") \
+    .config("spark.driver.host", "spark-driver") \
+    .config("spark.driver.bindAddress", "0.0.0.0") \
+    .config("spark.executor.memory", "2g") \
+    .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
+    .config("spark.hadoop.fs.s3a.access.key", "minio") \
+    .config("spark.hadoop.fs.s3a.secret.key", "minio123") \
+    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+    .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.4") \
+    .getOrCreate()
 
     try:
         start_time = time.time()
